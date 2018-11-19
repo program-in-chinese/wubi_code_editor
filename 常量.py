1e88f3431937aff8ec291740029cdaf4a5d3d751
CNMan <cnnexus5x@gmail.com>
2018-11-19 12:12:47 +0800

    增加显示BabelStone Han字形

--- 常量.py
+++ 常量.py
@@ -24,6 +24,7 @@ from collections import OrderedDict
 字体名_汉仪字典宋 = "汉仪字典宋"
 字体名_汉仪仿宋 = "汉仪仿宋"
 字体名_方正楷体S = "方正楷体S"
+字体名_BabelStoneHan = "BabelStoneHan"
 
 字体名_細明體 = "細明體"
 字体名_細明體_HKSCS = "細明體_HKSCS"
@@ -39,6 +40,7 @@ from collections import OrderedDict
     字体名_汉仪字典宋: "HYZiDianSong/",
     字体名_汉仪仿宋: "HYFangSong/",
     字体名_方正楷体S: "FZKaiS/",
+    字体名_BabelStoneHan: "BabelStoneHan/",
     字体名_細明體: "MingLiU/",
     字体名_細明體_HKSCS: "MingLiU_HKSCS/",
     字体名_方正楷体T: "FZKaiT/",
@@ -50,7 +52,7 @@ from collections import OrderedDict
 无字体图片 = "images/NoneFontGlyph.gif"
 
 按地区名取字体列表 = OrderedDict([
-    ("中国大陆", [字体名_中易宋体, 字体名_中华书局宋体, 字体名_汉仪字典宋, 字体名_汉仪仿宋, 字体名_方正楷体S]),
+    ("中国大陆", [字体名_中易宋体, 字体名_中华书局宋体, 字体名_汉仪字典宋, 字体名_汉仪仿宋, 字体名_方正楷体S, 字体名_BabelStoneHan]),
     ("中国台港澳", [字体名_細明體, 字体名_細明體_HKSCS, 字体名_方正楷体T, 字体名_全字庫正宋體, 字体名_全字庫正楷體]),
     ("日本", [字体名_花園明朝])])
 
