8f6925148e679c9fae7a47fd09f4586b4cbc1b66
CNMan <nklijl@mail.nankai.edu.cn>
2022-10-21 23:33:54 +0800

    支持CJK-H

--- 常量.py
+++ 常量.py
@@ -3,16 +3,27 @@ from collections import OrderedDict
 
 源数据路径 = "UnicodeCJK-WuBi/"
 
-# https://github.com/CNMan/UnicodeCJK-WuBi/pull/2#issuecomment-424330083
-# 4E00..9FFF	CJK
-# 3400..4DBF	CJK-A
-# 20000..2A6DF	CJK-B
-# 2A700..2B73F	CJK-C
-# 2B740..2B81F	CJK-D
-# 2B820..2CEAF	CJK-E
-# 2CEB0..2EBEF	CJK-F
+# https://www.unicode.org/Public/UCD/latest/ucd/Blocks.txt
+# 2E80..2EFF; CJK Radicals Supplement
+# 2F00..2FDF; Kangxi Radicals
+# 3000..303F; CJK Symbols and Punctuation
+# 31C0..31EF; CJK Strokes
+# 3200..32FF; Enclosed CJK Letters and Months
+# 3300..33FF; CJK Compatibility
+# 3400..4DBF; CJK Unified Ideographs Extension A
+# 4E00..9FFF; CJK Unified Ideographs
+# F900..FAFF; CJK Compatibility Ideographs
+# FE30..FE4F; CJK Compatibility Forms
+# 20000..2A6DF; CJK Unified Ideographs Extension B
+# 2A700..2B73F; CJK Unified Ideographs Extension C
+# 2B740..2B81F; CJK Unified Ideographs Extension D
+# 2B820..2CEAF; CJK Unified Ideographs Extension E
+# 2CEB0..2EBEF; CJK Unified Ideographs Extension F
+# 2F800..2FA1F; CJK Compatibility Ideographs Supplement
+# 30000..3134F; CJK Unified Ideographs Extension G
+# 31350..323AF; CJK Unified Ideographs Extension H
 # 按码大小范围排序
-源数据文件 = ["CJKRadicalsSupplement.txt", "KangxiRadicals.txt", "CJKStrokes.txt", "CJK-A.txt", "CJK.txt", "CJKCompatibilityIdeographs.txt", "CJK-B.txt", "CJK-C.txt", "CJK-D.txt", "CJK-E.txt", "CJK-F.txt", "CJKCompatibilityIdeographsSupplement.txt", "CJK-G.txt"]
+源数据文件 = ["CJKRadicalsSupplement.txt", "KangxiRadicals.txt", "CJKStrokes.txt", "CJK-A.txt", "CJK.txt", "CJKCompatibilityIdeographs.txt", "CJK-B.txt", "CJK-C.txt", "CJK-D.txt", "CJK-E.txt", "CJK-F.txt", "CJKCompatibilityIdeographsSupplement.txt", "CJK-G.txt", "CJK-H.txt"]
 # 暂时只指出导出到一个文件
 修改后文件 = "CJK-所有.txt"
 
